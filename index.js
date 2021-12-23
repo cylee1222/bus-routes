34f6d3a17adf34a54934e585c31abd69dc4429fc
Lee Chun Yin <u3555604@connect.hku.hk>
2021-12-23 16:55:19 +0800

    ejs

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.set('view engine','ejs')
+app.set('views', './src/views')
+
+const router = require('./src/routes/index')
+app.use('/', router)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT)
