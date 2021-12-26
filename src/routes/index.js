1f4ad69c89a5dfc592e1bc1bc6188ebc86dc0cb4
Lee Chun Yin <u3555604@connect.hku.hk>
2021-12-26 09:51:57 +0800

    map

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,9 +5,29 @@ router.get('', async (req, res) => {
     res.render('index')
 })
 
+async function loadAllRoutes(){
+    const res = await fetch('https://data.etabus.gov.hk/v1/transport/kmb/route').catch(err => console.error('Cannot fetch data'))
+    const res_json = await res.json()
+    const filter_route = {
+        "service_type": "1",
+        "bound": "O"
+    }
+    const routes = res_json.data.filter(route => {
+        for(let key in filter_route){
+            if(route[key] != filter_route[key]){
+                return false
+            }
+        }
+        return true
+    })
+    return routes
+}
+
 router.get('/:rnum', async (req, res) => {
     let rnum = req.params.rnum
-    res.render('route', {rnum: rnum.toUpperCase()})
+    const routes = await loadAllRoutes()
+
+    res.render('route', {rnum: rnum.toUpperCase(), routes: routes})
 })
 
 router.get('/stop/:stop_id', async (req, res) => {
