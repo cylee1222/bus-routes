5f5316542afda7b2aaa26ab8edcf7ab87e78d6f5
Lee Chun Yin <u3555604@connect.hku.hk>
2021-12-27 18:19:36 +0800

    Route Link

--- public/js/wapp.js
+++ public/js/wapp.js
@@ -32,62 +32,95 @@ async function loadAllRoutes(){
 async function renderRoutes(routes){
     let div = document.getElementById('routes')
     div.innerHTML = '<p>Route</p><p>起點站/終點站</p><p>Origin/Destination</p>'
-    for(let i = 0; i < routes.length; i++){
+    for(const element of routes) {
         let a = document.createElement('a')
-        a.href = `/${routes[i].route}`
-        a.textContent = routes[i].route
+        a.href = `/${element.route}`
+        a.textContent = element.route
         render(a)
 
         
-        let sym = routes[i].dest_en.includes('(CIRCULAR)') ? '&#8634;' : '&#8596;'
+        let sym = element.dest_en.includes('(CIRCULAR)') ? '&#8634;' : '&#8596;'
 
-        let stop_result = await loadStop(routes[i].route)
-        let id1 = stop_result[0].stop
-        let id2 = stop_result[stop_result.length-1].stop
+        // let stop_result = await loadStop(element.route)
+        // let id1 = stop_result[0].stop
+        // let id2 = stop_result[stop_result.length-1].stop
 
         let p1 = document.createElement('p')
-        if(sym != '&#8634;'){  
-            p1.innerHTML += `<a href='/stop/${id1}'>${routes[i].orig_tc}</a>&nbsp;${sym}&nbsp;<a href='/stop/${id2}'>${routes[i].dest_tc}</a>`
-        }else{
-            // circular stop not given
-            p1.innerHTML += `<a href='/stop/${id1}'>${routes[i].orig_tc}</a>&nbsp;${sym}&nbsp;${routes[i].dest_tc}`
-        }
+        p1.innerHTML += `${element.orig_tc}&nbsp;${sym}&nbsp;${element.dest_tc}`
+        // if(sym != '&#8634;'){  
+        //     p1.innerHTML += `<a href='/stop/${id1}'>${element.orig_tc}</a>&nbsp;${sym}&nbsp;<a href='/stop/${id2}'>${element.dest_tc}</a>`
+        // }else{
+        //     // circular stop not given
+        //     p1.innerHTML += `<a href='/stop/${id1}'>${element.orig_tc}</a>&nbsp;${sym}&nbsp;${element.dest_tc}`
+        // }
         div.appendChild(p1)
 
 
         let p2 = document.createElement('p')
-        if(sym != '&#8634;'){  
-            p2.innerHTML += `<a href='/stop/${id1}'>${routes[i].orig_en}</a>&nbsp;${sym}&nbsp;<a href='/stop/${id2}'>${routes[i].dest_en}</a>`
-        }else{
-            // circular stop not given
-            p2.innerHTML += `<a href='/stop/${id1}'>${routes[i].orig_en}</a>&nbsp;${sym}&nbsp;${routes[i].dest_en}`
-        }
+        p2.innerHTML += `${element.orig_en}&nbsp;${sym}&nbsp;${element.dest_en}`
+        // if(sym != '&#8634;'){  
+        //     p2.innerHTML += `<a href='/stop/${id1}'>${element.orig_en}</a>&nbsp;${sym}&nbsp;<a href='/stop/${id2}'>${element.dest_en}</a>`
+        // }else{
+        //     // circular stop not given
+        //     p2.innerHTML += `<a href='/stop/${id1}'>${element.orig_en}</a>&nbsp;${sym}&nbsp;${element.dest_en}`
+        // }
         div.appendChild(p2)
     }
 }
 
-async function searchRoute(query){
-    const routes = await loadAllRoutes()
-    let result = routes.filter(element => element.route.indexOf(query) === 0)
-    await renderRoutes(result)
-}
-
-async function checkInput(e){
-    if(e.target.value==''){
-        renderRoutes(await loadAllRoutes())
-    }else{
-        searchRoute(e.target.value)
-    }
-}
-
 async function main(){
-    await renderRoutes(await loadAllRoutes())
+    const allRoutes = await loadAllRoutes()
+    const routes = renderRoutes(allRoutes)
 
     let s = document.getElementById('search')
     s.addEventListener('input', e => {
         e.target.value = e.target.value.toUpperCase()
-        checkInput(e)
+        if(e.target.value==''){
+            renderRoutes(allRoutes)
+        }else{
+            let result = allRoutes.filter(element => element.route.indexOf(e.target.value) === 0)
+            renderRoutes(result)
+        }
     })
+
+
 }
 
 window.onload = main
+
+// async function renderRoutes(routes){
+//     let div = document.getElementById('routes')
+//     div.innerHTML = '<p>Route</p><p>起點站/終點站</p><p>Origin/Destination</p>'
+//     for(const element of routes) {
+//         let a = document.createElement('a')
+//         a.href = `/${element.route}`
+//         a.textContent = element.route
+//         render(a)
+
+        
+//         let sym = element.dest_en.includes('(CIRCULAR)') ? '&#8634;' : '&#8596;'
+
+//         let stop_result = await loadStop(element.route)
+//         let id1 = stop_result[0].stop
+//         let id2 = stop_result[stop_result.length-1].stop
+
+//         let p1 = document.createElement('p')
+//         if(sym != '&#8634;'){  
+//             p1.innerHTML += `<a href='/stop/${id1}'>${element.orig_tc}</a>&nbsp;${sym}&nbsp;<a href='/stop/${id2}'>${element.dest_tc}</a>`
+//         }else{
+//             // circular stop not given
+//             p1.innerHTML += `<a href='/stop/${id1}'>${element.orig_tc}</a>&nbsp;${sym}&nbsp;${element.dest_tc}`
+//         }
+//         div.appendChild(p1)
+
+
+//         let p2 = document.createElement('p')
+//         if(sym != '&#8634;'){  
+//             p2.innerHTML += `<a href='/stop/${id1}'>${element.orig_en}</a>&nbsp;${sym}&nbsp;<a href='/stop/${id2}'>${element.dest_en}</a>`
+//         }else{
+//             // circular stop not given
+//             p2.innerHTML += `<a href='/stop/${id1}'>${element.orig_en}</a>&nbsp;${sym}&nbsp;${element.dest_en}`
+//         }
+//         div.appendChild(p2)
+//     }
+// }
